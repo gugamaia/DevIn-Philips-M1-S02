86a1df80337fcc37d6d0284300435ae9156b7396
Gustavo Maia <gustavojv_maia@outlook.com>
2022-10-14 00:14:21 -0300

    inicio do exercicio 3

---
+++ exercícios/03/script.js
@@ -0,0 +1,16 @@
+// Utilizando estrutura de repetição realize a tabuada do número x e exiba ela da seguinte forma:
+// 2 x 0 = 0
+// 2 x 1 = 2
+// ....
+// 2 x 10 = 20
+
+// O número para realizar a tabuada deve ser solicitado via prompt e o resultado apresentado via alert.
+
+// dica: de uma olhadinha no for.
+
+// Realize o commit do exercício resolvido.
+let numeroInformado = prompt('Informe um número válido');
+
+function tabuadaCalcular() {
+    for()
+}
